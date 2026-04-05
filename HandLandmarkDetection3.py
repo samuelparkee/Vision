721fb2ff8b46f7019e724097882b0bdbfd481e2d
samuelparkee <samuel.park.ee@gmail.com>
2026-04-04 23:30:30 -0700

    Create HandLandmarkDetection3.py
    - flipped the frame because i didn't like that it wasn't mirrored when doing imshow()
    - changed print_result() to store_result() and added global variable latest_result to use draw_landmarks_on_image() function
    - in draw_landmarks_on_image() function swapped labels to match hands because the frame was flipped

---
+++ HandLandmarkDetection3.py
@@ -0,0 +1,107 @@
+import mediapipe as mp
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
+import numpy as np
+
+import cv2
+import time
+
+model_path = 'hand_landmarker.task'
+
+BaseOptions = mp.tasks.BaseOptions
+HandLandmarker = mp.tasks.vision.HandLandmarker
+HandLandmarkerOptions = mp.tasks.vision.HandLandmarkerOptions
+HandLandmarkerResult = mp.tasks.vision.HandLandmarkerResult
+VisionRunningMode = mp.tasks.vision.RunningMode
+
+mp_hands = mp.tasks.vision.HandLandmarksConnections
+mp_drawing = mp.tasks.vision.drawing_utils
+mp_drawing_styles = mp.tasks.vision.drawing_styles
+
+MARGIN = 10  # pixels
+FONT_SIZE = 1
+FONT_THICKNESS = 1
+HANDEDNESS_TEXT_COLOR = (88, 205, 54)  # vibrant green
+
+latest_result = None
+
+# Create a hand landmarker instance with the live stream mode:
+def store_result(result: HandLandmarkerResult, output_image: mp.Image, timestamp_ms: int):
+    global latest_result
+    latest_result = result
+    print('hand landmarker result: {}'.format(latest_result))
+
+
+options = HandLandmarkerOptions(
+    base_options=BaseOptions(model_asset_path=model_path),
+    running_mode=VisionRunningMode.LIVE_STREAM,
+    result_callback=store_result,
+    num_hands=2)
+
+
+# ==================================================================================================================================================================
+def draw_landmarks_on_image(rgb_image, detection_result):
+    hand_landmarks_list = detection_result.hand_landmarks
+    handedness_list = detection_result.handedness
+    annotated_image = np.copy(rgb_image)
+
+    # Loop through the detected hands to visualize.
+    for idx in range(len(hand_landmarks_list)):
+        hand_landmarks = hand_landmarks_list[idx]
+        handedness = handedness_list[idx]
+
+        # Draw the hand landmarks.
+        mp_drawing.draw_landmarks(
+            annotated_image,
+            hand_landmarks,
+            mp_hands.HAND_CONNECTIONS,
+            mp_drawing_styles.get_default_hand_landmarks_style(),
+            mp_drawing_styles.get_default_hand_connections_style())
+
+        # Get the top left corner of the detected hand's bounding box.
+        height, width, _ = annotated_image.shape
+        x_coordinates = [landmark.x for landmark in hand_landmarks]
+        y_coordinates = [landmark.y for landmark in hand_landmarks]
+        text_x = int(min(x_coordinates) * width)
+        text_y = int(min(y_coordinates) * height) - MARGIN
+
+        label = handedness[0].category_name
+        if label == "Right":
+            label = "Left"
+        elif label == "Left":
+            label = "Right"
+
+        # Draw handedness (left or right hand) on the image.
+        cv2.putText(annotated_image, label,
+                    (text_x, text_y), cv2.FONT_HERSHEY_DUPLEX,
+                    FONT_SIZE, HANDEDNESS_TEXT_COLOR, FONT_THICKNESS, cv2.LINE_AA)
+
+    return annotated_image
+
+
+# ==================================================================================================================================================================
+cap = cv2.VideoCapture(0)
+if not cap.isOpened():
+    print("Cannot open camera")
+    exit()
+with HandLandmarker.create_from_options(options) as landmarker:
+    frame_timestamp_ms = time.time_ns() // 1000000
+    while True:
+        ret, frame = cap.read()
+        frame = cv2.flip(frame, 1)
+        if not ret:
+            print("Can't receive frame. Exiting...")
+            break
+        frame_timestamp_ms += 1
+        mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
+        landmarker.detect_async(mp_image, frame_timestamp_ms)
+        if latest_result is not None:
+            marked_image = draw_landmarks_on_image(frame, latest_result)
+            cv2.imshow("frame", marked_image)
+        else:
+            cv2.imshow('frame', frame)
+
+        if cv2.waitKey(1) == ord('q'):
+            break
+cap.release()
+cv2.destroyAllWindows()
