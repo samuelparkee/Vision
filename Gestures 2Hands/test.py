653c3e3ff2ecbb248f5d34dc748195bdff8510bf
spark <samuel.park.ee@gmail.com>
2026-07-20 22:28:46 -0700

    changed the test file from the original gesturedetection to the one with a main

--- Gestures 2Hands/test.py
+++ Gestures 2Hands/test.py
@@ -1,5 +1,5 @@
 import os
-from gesturedetection_2hands import get_keyboard_labels
+from gesturedetection_2hands_withmain import get_keyboard_labels
 
 KEYBOARD_LABELS_FILE_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), "keyboard_labels.txt")
 KEYBOARD_LABELS = []
